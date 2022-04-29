ce45ce42c0e353d071e40cb081beb9b17b1d50ab
Zeyang Lin <4020306+linzeyang@users.noreply.github.com>
2022-04-29 12:08:57 +0800

    FASP-3: Complete tutorial - Extra Data Types (#13)

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from datetime import datetime, time, timedelta
 from decimal import Decimal
 from enum import Enum
 from http import HTTPStatus
 from typing import Dict, List, Optional
+from uuid import UUID
 
 from fastapi import Body, FastAPI, Header, HTTPException, Path, Query
 from pydantic import BaseModel, Field, HttpUrl
@@ -223,3 +225,24 @@ def create_multiple_images(images: List[Image]):
 @app.post("/index-weights/", status_code=HTTPStatus.CREATED)
 def create_index_weights(weights: Dict[int, Decimal]):
     return weights
+
+
+@app.post("/event/{event_id}", status_code=HTTPStatus.CREATED)
+def create_event(
+    event_id: UUID,
+    start_datetime: Optional[datetime] = Body(None),
+    end_datetime: Optional[datetime] = Body(None),
+    repeat_at: Optional[time] = Body(None),
+    process_after: Optional[timedelta] = Body(None),
+):
+    start_process = start_datetime + process_after
+    duration = end_datetime - start_process
+
+    return {
+        "event_id": event_id,
+        "start_datetime": start_datetime,
+        "end_datetime": end_datetime,
+        "repeat_at": repeat_at,
+        "process_after": process_after,
+        "duration": duration,
+    }
